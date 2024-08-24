66222724a9654b0d9a37b6f99998dc49b3ebb777
Abhishek <abhishekkumar@amritaelearning.org>
2024-08-24 18:27:30 +0530

    Enhancements/UI changes in AppBar 1.Added background color to appbar and removed the divider

--- lib/config/customThemes/MyAppBar.dart
+++ lib/config/customThemes/MyAppBar.dart
@@ -16,18 +16,20 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         IconButton(icon: Icon(Icons.list), onPressed: () {}),
         TextButton(onPressed: () {}, child: Text("Patient List", style: TextStyle(fontSize: 16, color: Colors.black))),
         IconButton(icon: Icon(Icons.schedule_outlined), onPressed: () {}),
-        TextButton(onPressed: () {}, child: Text("Scheuler", style: TextStyle(fontSize: 16, color: Colors.black))),
+        TextButton(onPressed: () {}, child: Text("Scheduler", style: TextStyle(fontSize: 16, color: Colors.black))),
         IconButton(icon: Icon(Icons.settings), onPressed: () {}),
         TextButton(onPressed: () {}, child: Text("Settings", style: TextStyle(fontSize: 16, color: Colors.black))),
 
         //IconButton(icon: Icon(Icons.info_outline), onPressed: () {}),
       ],
-      bottom:  PreferredSize(
-        preferredSize: Size.fromHeight(1.0),
-        // Set the height of the divider
-        child: Divider(
-            color: Colors.blue.shade50), // Divider below the app bar title
-      ),
+      backgroundColor: Colors.grey[100],
+      // bottom:  PreferredSize(
+      //   preferredSize: Size.fromHeight(1.0),
+      //   // Set the height of the divider
+      //   child: Divider(
+      //     thickness: 2,
+      //       color: Colors.blue.shade50), // Divider below the app bar title
+      // ),
     );
   }
 
